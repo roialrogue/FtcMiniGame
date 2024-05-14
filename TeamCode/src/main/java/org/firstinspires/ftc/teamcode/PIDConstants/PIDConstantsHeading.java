6e6a1a1b1702dd0a47432c582c441cd40fdf4d46
asher <asherplihal1@gamil.com>
2024-05-14 11:35:47 -0400

    First

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDConstants/PIDConstantsHeading.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDConstants/PIDConstantsHeading.java
@@ -11,9 +11,9 @@ import org.firstinspires.ftc.teamcode.Hardware;
 
 @Config
 public class PIDConstantsHeading {
-    public static double Kp = 6;  // Proportional gain
+    public static double Kp = 10;  // Proportional gain
     public static double Ki = 0.0;   // Integral gain ( KEEP THIS 0 )
-    public static double Kd = 5.5;   // Derivative gain
+    public static double Kd = .3;   // Derivative gain
     public static double referenceAngle = 0; //Angle
 
 }
